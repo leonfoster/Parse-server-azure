5b0dc973ed1b9f2ff5acc213241ab4ac98de2b06
Matt Mason <mamaso@microsoft.com>
2016-03-25 08:49:39 -0700

    Updated push adapter

--- cleanNodeModules.ps1
+++ cleanNodeModules.ps1
@@ -1,3 +1,4 @@
-Get-ChildItem -Recurse | where {$_.PSIsContainer} | Where-Object {$_.Name -match '^(test[s]?|samples|doc[s]?|obj|typings|example[s]?)$'} | Remove-Item -Recurse
-Get-ChildItem -Path node_modules -Recurse | where {!$_.PSIsContainer} | Where-Object {$_.Name -match '\.(coffee|md|png|jpg|pdb|travis\.yml|gitignore|npmignore|d\.ts)$'} | Remove-Item -Recurse
-Get-ChildItem -Path node_modules -Recurse | where {!$_.PSIsContainer} | Where-Object {$_.Name -match '\.*(test|LICENSE|example).*$'} | Remove-Item -Recurse
+$path = "node_modules"
+Get-ChildItem -Path $path -Recurse | where {$_.PSIsContainer} | Where-Object {$_.Name -match '^(test[s]?|samples|doc[s]?|obj|typings|example[s]?)$'} | Remove-Item -Recurse -Verbose
+Get-ChildItem -Path $path -Recurse | where {!$_.PSIsContainer} | Where-Object {$_.Name -match '\.(coffee|md|png|jpg|pdb|travis\.yml|gitignore|npmignore|d\.ts)$'} | Remove-Item -Recurse -Verbose
+Get-ChildItem -Path $path -Recurse | where {!$_.PSIsContainer} | Where-Object {$_.Name -match '\.*(test|LICENSE|example).*$'} | Remove-Item -Recurse -Verbose
